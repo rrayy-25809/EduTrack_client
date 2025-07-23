60f29c0b49756c52fadb2c49ee7c7b55418466fe
김민결 <139607260+tatatommy6@users.noreply.github.com>
2025-07-23 16:17:45 +0900

    커밋 수 늘리기

--- capture_qrcode.py
+++ capture_qrcode.py
@@ -11,13 +11,13 @@ class capture_qrcode:
         self.last_time = 0       # 마지막 인식 시간
 
         if not self.cap.isOpened():
-            print("❌ 카메라를 열 수 없습니다.")
+            print("카메라를 열 수 없습니다.")
             raise Exception("카메라를 열기 실패")
 
     def capture(self):
         ret, frame = self.cap.read()
         if not ret:
-            print("❌ 프레임을 읽을 수 없습니다.")
+            print("프레임을 읽을 수 없습니다.")
             raise Exception("프레임 읽기 실패")
 
         data = self.detector.detectAndDecode(frame)[1]
@@ -25,13 +25,13 @@ class capture_qrcode:
         if data:
             current_time = time.time()
             if data != last_detected or (current_time - last_time > 5):  # 5초 이내 중복 방지
-                print("\n✅ QR 코드 인식됨:")
+                print("QR 코드 인식됨:")
                 try:
                     parsed = json.loads(data)
                     for i, j in parsed.items():
                         print(f"  {i}: {j}")
                 except json.JSONDecodeError:
-                    print("⚠️ JSON 형식이 아닙니다.")
+                    print(" JSON 형식이 아닙니다.")
 
                 last_detected = data
                 last_time = current_time
